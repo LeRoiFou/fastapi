1a3b8cc1df5d5a0a348c643793e1d62e35a489dc
LeRoiFou <lrcompta@gmail.com>
2025-06-09 21:35:14 +0200

    indexB004 CSS

--- indexB004_essai.py
+++ indexB004_essai.py
@@ -1,35 +1,17 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.exceptions import RequestValidationError
-from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 import uvicorn
 
 # Instanciation de la sous-librairie FastAPI
 app = FastAPI()
 
-# Instanciation en variable du dossier templates récupéré (fichiers HTML récupérés)
+# Accès aux fichiers HTML du répertoire templates
 templates = Jinja2Templates(directory='templates')
 
-# Handler personnalisé pour les erreurs de validation
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    # Vérifie si la requête vient d’un formulaire
-    if request.headers.get("content-type", "").startswith(
-        "application/x-www-form-urlencoded"):
-        return templates.TemplateResponse(
-            "indexB004.html",  # template HTML
-            {
-                "request": request,
-                "error_message": "Veuillez entrer un nombre entier valide !",
-            },
-            status_code=HTTP_422_UNPROCESSABLE_ENTITY,
-        )
-    # Sinon, comportement JSON par défaut
-    return JSONResponse(
-        status_code=HTTP_422_UNPROCESSABLE_ENTITY,
-        content={"detail": "Erreur de validation", "errors": exc.errors()},
-    )
+# Accès au répertoire static contenant les fichiers CSS
+app.mount('/static/', StaticFiles(directory='static'), name='static')
 
 @app.get('/hello', # URL avec chemin spécifié
          response_class=HTMLResponse, # affichage en HTML
@@ -40,30 +22,53 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
          param request: à mentionner sinon erreur
          """,
          )
-async def get_hello(request:Request):
+async def get_hello(request: Request):
     return templates.TemplateResponse(
         'indexB004.html', # accès au fichier HTML
-        {'request': request, 'message': ''}, # Données à restituer
-        )
-    
-@app.post('/hello', # URL avec chemin spécifié
-         response_class=HTMLResponse, # affichage en HTML
-         summary="Page HTML avec restitution des données saisies",
-         description="""
-         Retour de la page HTML avec données saisies par l'utilisateur
-         
-         param request: à mentionner sinon erreur
-         args message: données saisies par l'utilisateur
-         """,
-         )
-async def post_hello(request: Request, message: int = Form(...)) -> str:
-    return templates.TemplateResponse(
-        'indexB004.html', # accès au fichier HTML
-        {'request': request, 'message': message}, # Données à restituer
-        )
+        { # Données à restituer
+            'request': request, # obligatoire pour une restitution en HTML
+            'message': '', # message de réponse
+            'error_message': '', # message d'erreur
+        },
+    )
 
+@app.post('/hello', # URL avec chemin spécifié
+          response_class=HTMLResponse, # affichage en HTML
+          summary="Page HTML avec restitution des données saisies",
+          description="""
+          Retour de la page HTML avec données saisies par l'utilisateur
+          
+          param request: à mentionner sinon erreur
+          args message: données saisies par l'utilisateur de type str !!!
+         """,
+          )
+async def post_hello(request: Request, message: str = Form(...)) -> str:  
+    # La restitution est un entier
+    try:
+        # Conversion du message saisi de type str en type int
+        int_message = int(message)
+        
+        return templates.TemplateResponse(
+            'indexB004.html', # accès au fichier HTML
+            { # Données à restituer
+                'request': request, # obligatoire pour une restitution en HTML
+                'message': int_message, # message de réponse
+                'error_message': '', # message d'erreur
+            },
+        )
+        
+    # Erreur si la restitution n'est pas un entier
+    except ValueError:
+        return templates.TemplateResponse(
+            'indexB004.html', # accès au fichier HTML
+            { # Données à restituer
+                'request': request, # obligatoire pour une restitution en HTML
+                'message': '', # message de réponse
+                'error_message': 
+                    'Veuillez entrer un nombre entier valide !', # message d'erreur
+            },
+        )
 
 if __name__ == '__main__':
-    
     # Lancement du serveur local
     uvicorn.run(app)
